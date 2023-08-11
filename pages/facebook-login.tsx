9fd9f2c97cf16fc5cfc6bdf813639376eef5f02d
TrinhDuc997 <trinhxuanduc997@gmail.com>
2023-08-11 08:54:55 +0700

    Fix bug login with Facebook 3

--- pages/facebook-login.tsx
+++ pages/facebook-login.tsx
@@ -7,7 +7,7 @@ const PopupFacebookLoginPage: React.FC = () => {
 
   useEffect(() => {
     if (status === "unauthenticated" || status === "loading") {
-      signIn("facebook", { callbackUrl: "/facebook-login" });
+      signIn("facebook", { callbackUrl: "/facebook-login", redirect: true });
     } else {
       window.close();
     }
